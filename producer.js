f2af8ef33c322f7376a4e3b450e0ea1788d963a5
shashank189612 <shashanknm18@gmail.com>
2025-05-20 21:12:14 +0530

    admin configured

---
+++ producer.js
@@ -0,0 +1,21 @@
+const { kafka, Kafka } = require("kafkajs")
+
+
+send();
+
+async function send() {
+
+    try {
+
+        const producer = kafka.producer( )
+        
+    } catch (e) {
+        
+    }
+
+
+
+
+
+}
+
---
+++ topic.js
@@ -0,0 +1,37 @@
+// const kafka = require("kafkajs").kafka
+const { kafka, Kafka } = require("kafkajs")
+
+
+run();
+
+async function run() {
+
+    try {
+        const kafka = new Kafka({
+            "clientId": "myAPP",
+            "brokers": ["localhost:9092"]
+        })
+        // we need the admin interface to create topics
+        const admin = kafka.admin()
+        console.log("connecting ......... ")
+        await admin.connect()
+        console.log("connected successfully! ")
+
+
+        //A-M , N-Z
+        await admin.createTopics({
+            topics: [{
+                "topic": "users",
+                "numPartitions": 2
+            }]
+        })
+
+        await admin.disconnect()
+
+    } catch (ex) {
+        console.log(`Something bad happended ${ex}`)
+    }
+    finally {
+        process.exit()
+    }
+}
